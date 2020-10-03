c4583fd3e22e9f5e186d684ad00148d7f8a55ffa
Tazkia Afra <tazkia.afra@gmail.com>
2020-10-02 21:39:33 -0400

    commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Tazkia Afra on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
